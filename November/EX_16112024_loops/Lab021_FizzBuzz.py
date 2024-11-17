c924d2808664355b33b3eb0c83061ce0d7e1af0a
Roopa <roopa.pandu@gmail.com>
2024-11-17 17:09:49 +0900

    Loop lab

--- November/EX_16112024_loops/Lab021_FizzBuzz.py
+++ November/EX_16112024_loops/Lab021_FizzBuzz.py
@@ -1,9 +1,11 @@
 # code to print FIZZ if number is multiple of 3 print BUZZ id number is multiple of 5 and print FIZZBUZZ is number is not multiple of 3 or 5 .
 
 for a in range(1,100):
-    if a%3 == 0:
-        print("FIZZ")
+    if a%3 == 0 and a%5==0:
+        print("FIZZBUZZ")
     elif a%5 == 0:
         print("BUZZ")
+    elif a%3 == 0:
+        print("BUZZ")
     else:
-        print("FIZZBUZZ")
+        print(a)
